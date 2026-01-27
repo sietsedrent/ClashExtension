4461466f69e57449bc9d0013dea2f1288fd44b21
Sietse Drent (student) <s1113887@student.windesheim.nl>
2026-01-27 17:31:20 +0100

    Initial Commit

---
+++ ClashExtension/ClashExtension/Controllers/ClanController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClashExtensionAPI.Controllers
+{
+    [Route("api/Clans")]
+    [ApiController]
+    public class ClanController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public ClanController(IHttpClientFactory _client)
+        {
+            _httpClient = _client.CreateClient("coc");
+        }
+
+        [HttpGet("GetClans")]
+        public async Task<string> GetClans()
+        {
+            return "test";
+        }
+    }
+}
---
+++ ClashExtension/ClashExtension/Program.cs
@@ -0,0 +1,43 @@
+
+namespace ClashExtensionAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddHttpClient("coc", client =>
+            {
+                client.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue(
+                        "Bearer",
+                        builder.Configuration["ClashofClans:key"]);
+            });
+            builder.Services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
